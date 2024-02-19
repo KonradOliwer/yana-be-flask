22308029abd477246cdf4988166dbeac4b91a5a2
Konrad <kcomrp@gmail.com>
2024-02-19 18:20:13 +0100

    fix serialization

--- opennote/notes/notes.py
+++ opennote/notes/notes.py
@@ -22,6 +22,11 @@ class ErrorResponseCodes(Enum):
 class ErrorResponse(BaseModel):
     code: ErrorResponseCodes
 
+    def to_dict(self):
+        return {
+            'code': self.code.value
+        }
+
 
 class CreateNoteDTO(BaseModel):
     name: str = Field(..., max_length=50)
@@ -38,9 +43,13 @@ class NoteDTO(CreateNoteDTO):
 
 @notes_bluprint.get('/')
 def get_all_notes() -> tuple[Response, int]:
-    # TODO fix list serialization
-    #  this is a hack and will not work if jsonify can't handle serialization of object ex. if model would use enums"
-    return jsonify([NoteDTO.from_note(note).__dict__ for note in db.session.query(Note).all()]), 200
+    name = request.args.get('name')
+    if name:
+        notes = db.session.query(Note).filter(Note.name == name).all()
+        return jsonify([NoteDTO.from_note(note).__dict__ for note in notes]), 200
+
+    notes = db.session.query(Note).all()
+    return jsonify([NoteDTO.from_note(note).__dict__ for note in notes]), 200
 
 
 @notes_bluprint.post('/')
@@ -54,9 +63,9 @@ def create_note() -> tuple[Response, int]:
         )
         db.session.add(new_note)
         db.session.commit()
-        return jsonify(NoteDTO.from_note(new_note).json()), 201
+        return jsonify(NoteDTO.from_note(new_note).__dict__), 201
     except IntegrityError:
-        return jsonify(ErrorResponse(code=ErrorResponseCodes.ALREADY_EXISTS).json()), 400
+        return jsonify(ErrorResponse(code=ErrorResponseCodes.ALREADY_EXISTS).to_dict()), 400
 
 
 @notes_bluprint.get('/<uuid:id>')
@@ -64,14 +73,14 @@ def get_note(id: uuid) -> tuple[Response, int]:
     note = db.session.query(Note).get(id)
     if note is None:
         return jsonify({'error': 'Note not found'}), 404
-    return NoteDTO.from_note(note).to_json(), 200
+    return jsonify(NoteDTO.from_note(note).__dict__), 200
 
 
 @notes_bluprint.put('/<uuid:id>')
 def edit_note(id: uuid) -> tuple[Response, int]:
     request_dto = NoteDTO(**request.get_json())
     if request_dto.id != id:
-        return jsonify(ErrorResponse(code=ErrorResponseCodes.NOT_MATCHING_URL_ID).json()), 400
+        return jsonify(ErrorResponse(code=ErrorResponseCodes.NOT_MATCHING_URL_ID).to_dict()), 400
 
     persisted_note = db.session.query(Note).get(id)
     if persisted_note is not None:
@@ -80,16 +89,16 @@ def edit_note(id: uuid) -> tuple[Response, int]:
         persisted_note.name = request_dto.name
         persisted_note.content = request_dto.content
         db.session.commit()
-        return jsonify(NoteDTO.from_note(persisted_note).json()), 200
+        return jsonify(NoteDTO.from_note(persisted_note).__dict__), 200
     else:
-        return jsonify(ErrorResponse(code=ErrorResponseCodes.NOT_FOUND).json()), 404
+        return jsonify(ErrorResponse(code=ErrorResponseCodes.NOT_FOUND).to_dict()), 404
 
 
 @notes_bluprint.delete('/<uuid:id>')
 def delete_note(id: uuid) -> tuple[Response, int]:
     note = db.session.query(Note).get(id)
     if note is None:
-        return jsonify(ErrorResponse(code=ErrorResponseCodes.NOT_FOUND).json()), 404
+        return jsonify(ErrorResponse(code=ErrorResponseCodes.NOT_FOUND).to_dict()), 404
 
     db.session.delete(note)
     db.session.commit()
@@ -98,4 +107,4 @@ def delete_note(id: uuid) -> tuple[Response, int]:
 
 @notes_bluprint.errorhandler(IntegrityError)
 def handle_exception(e) -> tuple[Response, int]:
-    return jsonify(ErrorResponse(code=ErrorResponseCodes.WRITE_ERROR).json()), 400
+    return jsonify(ErrorResponse(code=ErrorResponseCodes.WRITE_ERROR).to_dict()), 400
